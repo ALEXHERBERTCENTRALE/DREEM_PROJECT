cf4d54936fb683d78b9593f24c62814d5f467d00
Baptiste-Turpin <t.baptiste0@gmail.com>
2018-12-02 20:02:58 +0100

    Update of feature_extraction.py to choose which signals are to be considered with wich methods to extract features.

--- feature_extraction.py
+++ feature_extraction.py
@@ -17,6 +17,14 @@ def mobilMean(signal , interval_width):  #use an odd number as interval_width, o
 
 ## methodOne(s) : feature-extraction methods
 
+def distanceMinMax( list_freq = None , param = None , rep_dim_feature_per_signal = False):  # param = [ interval_width ]
+    # Returns the sum of distances betwwen the minimum and the maximum on a given set of intervals. Intended to be used with the temporal signal
+    if rep_dim_feature_per_signal:
+        return 1
+    interval_width, = param
+    i_max = len(list_freq)//interval_width
+    return [ sum( np.linalg.norm( np.array([np.argmax(list_freq[i*interval_width:(i+1)*interval_width]) , max(list_freq[i*interval_width:(i+1)*interval_width])]) - np.array( [np.argmin(list_freq[i*interval_width:(i+1)*interval_width]) , min(list_freq[i*interval_width:(i+1)*interval_width])]) )  for i in range(i_max) ) ]  
+
 def maxAmpOne(list_freq = None, param = None , rep_dim_feature_per_signal = False):  # param useless
     # Returns the maximum amplitude of a list of frequencies
     if rep_dim_feature_per_signal:
@@ -99,7 +107,7 @@ def indexMaxAmpOne(list_freq = None , param = None , rep_dim_feature_per_signal 
         return 1
     interval_width, = param
     mobil_mean_list_freq = mobilMean(list_freq , interval_width)
-    return [mobil_mean_list_freq.index(max(mobil_mean_list_freq))]
+    return [np.argmax(mobil_mean_list_freq)]
 
 def indexMaxAmpFastOne(list_freq = None , param = None , rep_dim_feature_per_signal = False):  # param = [ interval_width ]
     # Give the index of maximum amplitude, for the data averaged over fixed intervals. Probably way faster than indexMaxAmpOne, though less accurate.
@@ -109,7 +117,7 @@ def indexMaxAmpFastOne(list_freq = None , param = None , rep_dim_feature_per_sig
     i_max = len(list_freq)//interval_width
     #side_interval = interval_width//2
     list_mean = list( np.average( list_freq[i*interval_width:(i+1)*interval_width]) for i in range(i_max) )
-    return [ list_mean.index(max(list_mean))*interval_width + interval_width//2 ]
+    return [ np.argmax(list_mean)*interval_width + interval_width//2 ]
 
 
 def meanDiffNeighbOne(list_freq = None , param = None , rep_dim_feature_per_signal = False):  #param is useless
@@ -149,30 +157,39 @@ def methodTestOne(list_freq = None, param = None, rep_dim_feature_per_signal = F
 
 ## Global feature-extraction methods
     
-def extractFeatureAll(h5file_freq , methodOne , param ):
+def extractFeatureAll(h5file_freq , methodOne , param , list_bool_extract_signal):
     # Method giving the design matrix of h5file, given a certain method
-    # Consider giving a list of methods and returninf the design matrix for all extracted features
+    # list_bool_extract_signal[i] contains 1 iff the ith signal (data field) must have its feature extracted for this methodOne. Must be of length : len(key_list)
     key_list = list(h5file_freq.keys())
+    key_list_extract = list( key_list[i] for i in range(len(key_list)) if list_bool_extract_signal[i] )
     nb_samples = len(h5file_freq[key_list[0]])
     dim_feature_per_signal = methodOne(rep_dim_feature_per_signal = True)
-    rep = np.zeros((nb_samples , len(h5file_freq)*dim_feature_per_signal))
+    rep = np.zeros((nb_samples , len(key_list_extract)*dim_feature_per_signal))
     
-    for k_id in range(len(key_list)):
-        k=key_list[k_id]
+    for k_id in range(len(key_list_extract)):
+        k=key_list_extract[k_id]
         rep[: ,k_id*dim_feature_per_signal:(k_id+1)*dim_feature_per_signal ] =  np.array( list(methodOne(h5file_freq[k][i] , param) for i in range(nb_samples)  ))
     return rep
 
-def extractMultiFeatureAll(h5file_freq , list_methodOne , list_param , save = False , name_save = None):
+def extractMultiFeatureAll(h5file_freq , list_methodOne , list_param , mat_bool_extract_signal , save = False , name_save = None):
     # Returns the concatenation of design matrices for a list of methods
+    # mat_bool_extract_signal[i] (ith row) contains list_bool_extract_signal for extractFeatureAll function. IE : mat_bool_extract_signal[i,j] is 1 iff for ith methodOne, jth signal must have its feature extracted. 
+    # mat_bool_extract_signal must be of size : (nb of methodOnes , len(key_list)  )
+    key_list = list(h5file_freq.keys())
     nb_samples = len(h5file_freq[list(h5file_freq.keys())[0]])
-    sum_dim_feature_per_signal = sum( methodOne(rep_dim_feature_per_signal = True) for methodOne in list_methodOne )
-    rep = np.zeros((nb_samples , len(h5file_freq)*sum_dim_feature_per_signal ))
+    #sum_dim_feature_per_signal = sum( methodOne(rep_dim_feature_per_signal = True) for methodOne in list_methodOne )
+    
+    list_key_list_extract = list( list( key_list[j] for j in range(len(key_list)) if mat_bool_extract_signal[i,j] ) for i in range(len(list_methodOne)) )
+    
+    sum_weighted_dim_feature_per_signal = sum( list_methodOne[i](rep_dim_feature_per_signal = True)*len(list_key_list_extract[i]) for i in range(len(list_methodOne)) )
+    
+    rep = np.zeros((nb_samples , sum_weighted_dim_feature_per_signal ))
     
     c = 0
     i = 0
     for methodOne in list_methodOne :
-        temp = len(h5file_freq)*methodOne(rep_dim_feature_per_signal = True)
-        rep[:,c:c+temp] = extractFeatureAll(h5file_freq , methodOne , list_param[i] )
+        temp = len(list_key_list_extract[i])*methodOne(rep_dim_feature_per_signal = True)
+        rep[:,c:c+temp] = extractFeatureAll(h5file_freq , methodOne , list_param[i] , mat_bool_extract_signal[i] )
         i+=1
         c+=temp
     
@@ -196,5 +213,8 @@ dico = {}
 dico["cle1"] = [np.arange(1,30) , np.random.normal(10,5,29) , np.arange(2,31) , np.random.uniform(12,45,29) ]
 dico["cle2"] = [np.arange(2,31), np.arange(1,30) , np.random.uniform(12,45,29) , np.random.normal(12,6,29)]
 dico["cle3"] = [np.random.normal(42,1,29), np.random.uniform(12,45,29), np.arange(2,31),np.arange(10,39)]
+
+mat_bool_test = np.array( [ [ 1,1,1 ] , [0,1,1] , [1,0,0] ] )
     
-    
+# print(extractMultiFeatureAll(dico , [nbPikesOne , methodTestOne ] , [ [5,4] , [0]  ] , mat_bool_test))
+# print(extractMultiFeatureAll(dico , [nbPikesOne , methodTestOne , stdDeviationNbOne] , [ [5,4] , [0] , [10] ] , mat_bool_test))
