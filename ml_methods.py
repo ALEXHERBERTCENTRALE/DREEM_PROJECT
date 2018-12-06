bf83d5ac71d9fcfccaaf02e7bb51c4e35f4feb6d
Alexandre Herbert <alexandre.herbert.85@gmail.com>
2018-12-06 20:13:54 +0100

    ...Almost...

--- ml_methods.py
+++ ml_methods.py
@@ -12,10 +12,10 @@ from sklearn import cluster
 from sklearn import svm
 
 def myKNeighborsClassifier(n_neighbors):
-    return neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=None)
+    return neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski', metric_params=None, n_jobs=1)
     
-def myLogisticRegression(C):
-    return linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=C, fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None, solver='lbfgs', max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=None)
+def myLogisticRegression(C, max_iter):
+    return linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=C, fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None, solver='lbfgs', max_iter=max_iter, multi_class='ovr', verbose=0, warm_start=False, n_jobs=None)
     
 def myDecisionTreeClassifier():
     return tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, class_weight=None, presort=False)
